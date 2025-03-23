b0988ee3240d3ddd0167fa2ccd99d830b065e9c4
rupeshtr78 <rupeshtr78@gmail.com>
2025-03-23 14:21:41 -0700

    refactor: update GetTags function to improve file handling and tag updates

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,19 @@
 #![allow(non_snake_case)]
 use anyhow::Result;
 use cli::{cli, SetLogLevel};
-mod file_renamer;
-use file_renamer::RenameFiles;
 mod cli;
+mod file_renamer;
 mod tagedit;
 
 fn main() -> Result<()> {
-    let args = cli()?;
+    // let args = cli()?;
 
     // let args = cli_dialog()?;
 
-    SetLogLevel(&args.loglevel)?;
+    // SetLogLevel(&args.loglevel)?;
 
-    RenameFiles(&args.path, &args.file_type, &args.current, &args.new)?;
-    // tagedit::read_tags("test/Ponveene.mp3")?;
+    // RenameFiles(&args.path, &args.file_type, &args.current, &args.new)?;
+    tagedit::GetTags("test/Ponveene.mp3")?;
 
     Ok(())
 }
--- src/tagedit.rs
+++ src/tagedit.rs
@@ -3,23 +3,35 @@ use id3::{Tag, TagLike, Version};
 use std::fs::{copy, File};
 
 pub fn GetTags(file_path: &str) -> Result<()> {
-    let temp_file = std::env::temp_dir().join("music.mp3");
-    copy(file_path, &temp_file)?;
+    let temp_file_path = std::env::temp_dir().join("music.mp3");
+    let file_path = std::path::Path::new(file_path);
+    let temp_file = std::path::Path::new("test/music.mp3");
+    copy(file_path, temp_file)?;
 
-    let mut tag = Tag::read_from_path(&temp_file).context("Failed to read tags")?;
+    let mut tag = Tag::read_from_path(temp_file).context("Failed to read tags")?;
 
-    tag.title().map(|t| println!("Title: {}", t));
-    tag.artist().map(|a| println!("Artist: {}", a));
-    tag.album().map(|a| println!("Album: {}", a));
-    tag.genre().map(|g| println!("Genre: {}", g));
+    println!("Before tag update:");
+    tag.title().map(|t| println!("  Title: {}", t));
+    tag.artist().map(|a| println!(" Artist: {}", a));
+    tag.album().map(|a| println!("  Album: {}", a));
+    tag.genre().map(|g| println!("  Genre: {}", g));
 
-    tag.set_album("Fancy Album Title");
+    tag.set_album("Tag updated Album Title2");
+    tag.set_artist("Tag updated Artist Name2");
+    // tag.set_title("Tag updated Title");
+    tag.set_genre("Tag updated Genre2");
 
     tag.write_to_path(temp_file, Version::Id3v24)?;
 
-    tag.album().map(|a| println!("Album: {}", a));
+    println!("After tag Update:");
+    tag.title().map(|t| println!("  Title: {}", t));
+    tag.artist().map(|a| println!(" Artist: {}", a));
+    tag.album().map(|a| println!("  Album: {}", a));
+    tag.genre().map(|g| println!("  Genre: {}", g));
 
-    // copy(&temp_file, file_path)?;
+    copy(&temp_file, file_path)?;
+    // delete temp file
+    std::fs::remove_file(temp_file)?;
 
     Ok(())
 }
